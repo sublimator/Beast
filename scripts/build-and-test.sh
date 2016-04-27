f5f272f86d0cabd7a4496414fb414b52e6fdb024
Nicholas Dudfield <ndudfield@gmail.com>
2016-04-27 14:58:07 +0700

    Add codecov

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -2,10 +2,23 @@
 # We use set -e and bash with -u to bail on first non zero exit code of any
 # processes launched or upon any unbound variable
 set -e
-__dirname=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 echo "using toolset: $CC"
 echo "using variant: $VARIANT"
 
 $BOOST_ROOT/bjam toolset=$CC variant=$VARIANT
+
+# Delete any coverage information from prior runs
+find . -name "*.gcda" | xargs rm
+# Keep these as these are the files created during compilation ;)
+# find . -name "*.gcno" | xargs rm
+
+# Run the tests again, so all test output is contiguous
+
 `find . -name "core_tests"`
 `find . -name "http_tests"`
+
+if [[ $VARIANT == "coverage" ]]; then
+  # We pass along -p to keep path segments so as to avoid collisions
+  codecov --gcov-args=-p --gcov-source-match='^((include/beast)|examples|test)'
+fi
